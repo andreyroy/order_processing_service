bf0ce430526e0e988d456cce224729a1d2867317
Gordei Skorobogatov <gordey.skor@mail.ru>
2024-11-23 15:28:01 +0000

    Add handlers base

--- postgresql/schemas/db_1.sql
+++ postgresql/schemas/db_1.sql
@@ -1,8 +1,8 @@
-DROP SCHEMA IF EXISTS hello_schema CASCADE;
+DROP SCHEMA IF EXISTS order_processing CASCADE;
 
-CREATE SCHEMA IF NOT EXISTS hello_schema;
+CREATE SCHEMA IF NOT EXISTS order_processing;
 
-CREATE TABLE IF NOT EXISTS hello_schema.users (
+CREATE TABLE IF NOT EXISTS order_processing.users (
     name TEXT PRIMARY KEY,
     count INTEGER DEFAULT(1)
 );
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,29 @@
+#include <userver/clients/dns/component.hpp>
 #include <userver/clients/http/component.hpp>
+#include <userver/components/component.hpp>
 #include <userver/components/minimal_server_component_list.hpp>
 #include <userver/server/handlers/ping.hpp>
 #include <userver/server/handlers/tests_control.hpp>
+#include <userver/storages/postgres/component.hpp>
 #include <userver/testsuite/testsuite_support.hpp>
 #include <userver/utils/daemon_run.hpp>
 
-#include "hello.hpp"
+#include "handlers/order_acquire/handler.hpp"
+#include "handlers/order_assign/handler.hpp"
+#include "handlers/order_cancel/handler.hpp"
 
 int main(int argc, char* argv[]) {
-  auto component_list = userver::components::MinimalServerComponentList()
-                            .Append<userver::server::handlers::Ping>()
-                            .Append<userver::components::TestsuiteSupport>()
-                            .Append<userver::components::HttpClient>()
-                            .Append<userver::server::handlers::TestsControl>();
-
-  pg_service_template::AppendHello(component_list);
+  auto component_list =
+      userver::components::MinimalServerComponentList()
+          .Append<userver::server::handlers::Ping>()
+          .Append<userver::components::TestsuiteSupport>()
+          .Append<userver::components::HttpClient>()
+          .Append<userver::server::handlers::TestsControl>()
+          .Append<userver::components::Postgres>("postgres-db-1")
+          .Append<userver::clients::dns::Component>()
+          .Append<handlers::order::cancel::Handler>()
+          .Append<handlers::order::assign::Handler>()
+          .Append<handlers::order::acquire::Handler>();
 
   return userver::utils::DaemonMain(argc, argv, component_list);
 }
